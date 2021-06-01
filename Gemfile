622fe7343c173ca0c7245e3e1f8c3c0d0f46a2c7
kazama <chipdale0119@gmail.com>
2021-06-01 18:27:36 +0900

    create user model

--- Gemfile
+++ Gemfile
@@ -10,6 +10,9 @@ gem 'pry-byebug'
 gem 'bootstrap-sass', '3.3.7'
 gem 'will_paginate',           '3.1.8'
 gem 'bootstrap-will_paginate', '1.0.0'
+# Use Active Model has_secure_password
+gem 'bcrypt', '~> 3.1.7'
+
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.4'
@@ -25,8 +28,6 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
-# Use Active Model has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_093533) do
+ActiveRecord::Schema.define(version: 2021_06_01_084341) do
 
   create_table "best_nines", force: :cascade do |t|
     t.string "title"
@@ -49,6 +49,15 @@ ActiveRecord::Schema.define(version: 2021_05_22_093533) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "field"
     t.string "league"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
