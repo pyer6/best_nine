966f2d2a7a4df27e24f372e3b828ce16f706fba9
kazama <chipdale0119@gmail.com>
2021-06-02 16:50:19 +0900

    create user's showpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get 'users/new'
+  # get 'users/show'
   root 'best_nines#index'
   resources :best_nines,  only: [:new, :create, :destroy]
+  resources :users
 end
