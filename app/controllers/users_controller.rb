f5dbb875bec65b2a02b8d8969403f801d6267969
kazama <chipdale0119@gmail.com>
2021-06-02 18:44:14 +0900

    create user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,27 @@
 class UsersController < ApplicationController
-  def new
-  end
-
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "登録が完了しました。"
+      redirect_to @user
+    else
+      flash[:danger] = "登録に失敗しました。"
+      render :new
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
                     format: {with: VALID_EMAIL_REGEX}, uniqueness: true
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 8, maximum: 32}
   has_secure_password
 end
